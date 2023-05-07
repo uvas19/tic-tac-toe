0f3df8905a9d2a83cb1bc6c15de67f8ad3a794b6
uvas <uvas19>
2023-05-06 17:16:19 -0700

    base clean code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Tic Tac Toe</h1>
+    <p>Sample react app</p>
+    </>
+  );
+}
+
+export default App;
